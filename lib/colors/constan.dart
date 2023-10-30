e617421e45b48326c512b46a4504ab2056988176
rafi-zimraan <raffizimraan27@gmail.com>
2023-10-30 11:13:30 +0700

    frist commit

---
+++ lib/colors/constan.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryColor = Color.fromARGB(255, 168, 9, 9);
+  static const secondaryColor = Color.fromARGB(255, 110, 7, 7);
+  static const accentColor = Color(0xFFB3E5FC);
+  // Tambahkan lebih banyak warna jika diperlukan
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:awesome_flutter/main_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Applikasi Wisata Indonesia',
+      theme: ThemeData(),
+      home: const MainScreen(),
+    );
+  }
+}
